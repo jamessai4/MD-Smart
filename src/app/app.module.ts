1b513aa481235ffadd38fd2cc5f11261b888c0fe
jamessai4 <48791720+jamessai4@users.noreply.github.com>
2021-12-06 14:22:47 +0700

    auth

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,29 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    AngularFireModule.initializeApp({
+      apiKey: "AIzaSyD9FBZOp77QIxeeaixVJLihXUCfvzwVi5M",
+      authDomain: "projectmdsmartbody.firebaseapp.com",
+      projectId: "projectmdsmartbody",
+      storageBucket: "projectmdsmartbody.appspot.com",
+      messagingSenderId: "960940584471",
+      appId: "1:960940584471:web:c25537c7a0df3b1711b652"
+    }),
+    AngularFirestoreModule,
+    AngularFireAuthModule
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import firebase from 'firebase/compat/app';
+
 @Component({
   selector: 'app-register',
   templateUrl: './register.page.html',
@@ -7,9 +11,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterPage implements OnInit {
 
-  constructor() { }
+  constructor(public auth: AngularFireAuth, private firestore: AngularFirestore) {
+    this.worksCollection = firestore.collection<any>('users')
+  }
 
   ngOnInit() {
   }
+  worksCollection: AngularFirestoreCollection<any>;
 
+  name: string;
+  age: string;
+  idcard: string;
+  email: string;
+  password: string;
+  cpassword: string;
+
+  submit() {
+    console.log(this.email)
+    console.log(this.password)
+    this.auth.createUserWithEmailAndPassword(this.email, this.password)
+      .then(res => {
+        console.log(res)
+        this.auth.currentUser.then(user => {
+
+          const id = this.firestore.createId();
+          const work = {
+            name: this.name,
+            age: this.age,
+            idcard: this.idcard,
+            uid: user.uid
+          }
+          this.worksCollection.doc(user.uid).set(work)
+            .then(() => {
+              this.name="";
+              this.age="";
+              this.idcard="";
+              this.email="";
+              this.password="";
+              this.cpassword="";
+            })
+
+        })
+      })
+  }
 }
