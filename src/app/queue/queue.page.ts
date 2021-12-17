1698808102cf8796db98dd61b4b8109109e1a840
Potae Dev <nametae4866@gmail.com>
2021-12-18 00:25:11 +0700

    แก้ปุ่มให้มีการกดยืนยันก่อนไปหน้าอื่น
    - หน้าสมัคร
    - หน้าจองคิว

--- src/app/queue/queue.page.ts
+++ src/app/queue/queue.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from '@angular/fire/compat/firestore';
+import { AlertController, NavController } from '@ionic/angular';
 import { Observable } from 'rxjs';
 import { AuthService } from '../auth.service';
 
@@ -12,7 +13,10 @@ export class QueuePage implements OnInit {
 
   constructor(
     private firestore: AngularFirestore,
-    public auth:AuthService) {
+    public auth:AuthService,
+    private nav: NavController,
+    public alertController: AlertController
+    ) {
     this.itemCollection = firestore.collection<any>('users' );
     this.items = this.itemCollection.doc(auth.user.uid).valueChanges();
   }
@@ -29,16 +33,40 @@ export class QueuePage implements OnInit {
   date:string;
 
 
-  submit(){
-    const item = {
-      symptom: this.symptom,
-      date: this.date
-    }
-    this.itemCollection.doc(this.auth.user.uid).update(item).then(()=>{
-      this.symptom=""
-      this.date=""
-    })
+  async submit(){
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'Confirm!',
+      message: 'Message <strong>text</strong>!!!',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: (blah) => {
+            console.log('Confirm Cancel: blah');
+          }
+        }, {
+          text: 'Okay',
+          handler: () => {
+            console.log('Confirm Okay');
+            const item = {
+              symptom: this.symptom,
+              date: this.date
+            }
+            this.itemCollection.doc(this.auth.user.uid).update(item).then(()=>{
+              this.symptom=""
+              this.date=""
+              this.nav.navigateRoot("/landing-page")
+            })
+          }
+        }
+      ]
+    });
+
+    await alert.present();
   }
   
 
 }
+
--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -1,5 +1,4 @@
 import { AlertController, NavController } from '@ionic/angular';
-
 import { Component, OnInit } from '@angular/core';
 
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
@@ -18,7 +17,7 @@ export class RegisterPage implements OnInit {
     private firestore: AngularFirestore,
     private nav: NavController,
     public alertController: AlertController
-    ) {
+  ) {
     this.worksCollection = firestore.collection<any>('users')
   }
 
@@ -33,33 +32,56 @@ export class RegisterPage implements OnInit {
   password: string;
   cpassword: string;
 
-  submit() {
-    console.log(this.email)
-    console.log(this.password)
-    this.auth.createUserWithEmailAndPassword(this.email, this.password)
-      .then(res => {
-        console.log(res)
-        this.auth.currentUser.then(user => {
-
-          const id = this.firestore.createId();
-          const work = {
-            name: this.name,
-            age: this.age,
-            idcard: this.idcard,
-            uid: user.uid
+  async submit() {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'Confirm!',
+      message: 'Message <strong>text</strong>!!!',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: (blah) => {
+            console.log('Confirm Cancel: blah');
           }
-          this.worksCollection.doc(user.uid).set(work)
-            .then(() => {
-              this.name="";
-              this.age="";
-              this.idcard="";
-              this.email="";
-              this.password="";
-              this.cpassword="";
-            })
+        }, {
+          text: 'Okay',
+          handler: () => {
+            console.log('Confirm Okay');
+            console.log(this.email)
+            console.log(this.password)
+            this.auth.createUserWithEmailAndPassword(this.email, this.password)
+              .then(res => {
+                console.log(res)
+                this.auth.currentUser.then(user => {
 
-        })
-      })
+                  const id = this.firestore.createId();
+                  const work = {
+                    name: this.name,
+                    age: this.age,
+                    idcard: this.idcard,
+                    uid: user.uid
+                  }
+                  this.worksCollection.doc(user.uid).set(work)
+                    .then(() => {
+                      this.name = "";
+                      this.age = "";
+                      this.idcard = "";
+                      this.email = "";
+                      this.password = "";
+                      this.cpassword = "";
+                      this.nav.navigateRoot("/login")
+                    })
+
+                })
+              })
+          }
+        }
+      ]
+    });
+
+    await alert.present();
   }
 
 }
