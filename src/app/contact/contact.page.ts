740a224025f9171f2ca00dda101b1ed969c417b2
Potae Dev <nametae4866@gmail.com>
2021-12-18 17:07:27 +0700

    แก้ 18/12/64 รอบ 3
    - แก้หน้า Contact
    - แก้ปุ่ม LINE PHONE ให้มีการแจ้งเตือน
    - เพิ่ม QR CODE ของ FACEBOOK
    - ตกแต่งให้สวยงาม

--- src/app/contact/contact.page.ts
+++ src/app/contact/contact.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-contact',
@@ -7,9 +8,63 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactPage implements OnInit {
 
-  constructor() { }
+  constructor(
+    public alertController: AlertController,
+  ) { 
+    
+  }
 
   ngOnInit() {
+  }
+
+  async submit1(){
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'ID : LINE',
+      message: 'ทางคลินิกยังไม่มี ID LINE จะมีในเร็วๆนี้ !',
+      buttons: [
+        {
+          text: 'ยกเลิก',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: (blah) => {
+            console.log('LOG : ยกเลิกการทำงาน');
+          }
+        }, {
+          text: 'ยืนยัน',
+          handler: () => {
+            console.log('LOG : ยืนยันการทำงาน');
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
+
+  async submit2(){
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'PHONE : NUMBER',
+      message: 'สามารถติดต่อได้ทุกวัน 10:00 - 21:00 น. !',
+      buttons: [
+        {
+          text: 'ยกเลิก',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: (blah) => {
+            console.log('LOG : ยกเลิกการทำงาน');
+          }
+        }, {
+          text: 'ยืนยัน',
+          handler: () => {
+            console.log('LOG : ยืนยันการทำงาน');
+          }
+        }
+      ]
+    });
+
+    await alert.present();
   }
 
 }
